6465cc937f156789f3c6260967a9ca7b2d17ba0b
fromdwlee <104008390+fromdwlee@users.noreply.github.com>
2022-05-28 22:16:51 +0900

    update momentum

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "0fbca77413005c1cb2b5ee69a7ef6b26";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(Response => Response.json())
+    .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
